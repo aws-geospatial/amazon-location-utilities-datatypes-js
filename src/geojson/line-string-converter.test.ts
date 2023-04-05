2a6aa6d7bc5456b1ab4cb24cae8eb7951b80830d
Eason Huang <yuxuanh@amazon.com>
2023-04-05 16:53:43 -0700

    Refactor the route converter to only use leg geometry.

--- src/geojson/line-string-converter.test.ts
+++ src/geojson/line-string-converter.test.ts
@@ -18,27 +18,6 @@ describe("convertLegsToLineString", () => {
     [9, 10],
   ];
 
-  it("should ignore StartPosition after the first leg assuming it should be the same as EndPosition of the previous leg.", () => {
-    const legs = [
-      {
-        Geometry: {
-          LineString: [positions[0], positions[1], positions[2]],
-        },
-        StartPosition: positions[0],
-        EndPosition: positions[2],
-      },
-      {
-        StartPosition: positions[2],
-        EndPosition: positions[3],
-      },
-      {
-        StartPosition: positions[3],
-        EndPosition: positions[4],
-      },
-    ] as Leg[];
-    const lineString = convertLegsToLineString(legs);
-    expectLineString(lineString).toHavePositions(positions);
-  });
   it("should ignore the first point in leg geometry after the first leg assuming it should be the same as EndPosition of the previous leg.", () => {
     const legs = [
       {
@@ -59,125 +38,19 @@ describe("convertLegsToLineString", () => {
     const lineString = convertLegsToLineString(legs);
     expectLineString(lineString).toHavePositions(positions);
   });
-  it("should throw an Error if the first Leg is missing StartPosition", () => {
-    const legs = [
-      {
-        EndPosition: positions[1],
-      },
-      {
-        StartPosition: positions[1],
-        EndPosition: positions[2],
-      },
-      {
-        StartPosition: positions[2],
-        EndPosition: positions[3],
-      },
-      {
-        StartPosition: positions[3],
-        EndPosition: positions[4],
-      },
-    ] as Leg[];
-    expect(() => convertLegsToLineString(legs)).toThrow("Leg 0 is missing StartPosition or EndPosition");
-  });
-  it("should throw an Error if the first Leg is missing EndPosition", () => {
+  it("should throw an Error if a leg is missing Geometry", () => {
     const legs = [
       {
         StartPosition: positions[0],
-      },
-      {
-        StartPosition: positions[1],
         EndPosition: positions[2],
       },
       {
         StartPosition: positions[2],
-        EndPosition: positions[3],
-      },
-      {
-        StartPosition: positions[3],
         EndPosition: positions[4],
       },
     ] as Leg[];
-    expect(() => convertLegsToLineString(legs)).toThrow("Leg 0 is missing StartPosition or EndPosition");
-  });
-  it("should throw an Error if a following Leg is missing StartPosition", () => {
-    const legs = [
-      {
-        StartPosition: positions[0],
-        EndPosition: positions[1],
-      },
-      {
-        StartPosition: positions[1],
-        EndPosition: positions[2],
-      },
-      {
-        StartPosition: positions[2],
-      },
-      {
-        EndPosition: positions[4],
-      },
-    ] as Leg[];
-    expect(() => convertLegsToLineString(legs)).toThrow("Leg 2 is missing EndPosition");
-  });
-  it("should skip first leg with undefined StartPosition if specified skipErrors", () => {
-    const legs = [
-      {
-        EndPosition: positions[1],
-      },
-      {
-        StartPosition: positions[1],
-        EndPosition: positions[2],
-      },
-      {
-        StartPosition: positions[2],
-        EndPosition: positions[3],
-      },
-      {
-        StartPosition: positions[3],
-        EndPosition: positions[4],
-      },
-    ] as Leg[];
-    const lineString = convertLegsToLineString(legs, true);
-    expectLineString(lineString).toHavePositions(positions.slice(1));
-  });
-  it("should skip first leg with undefined EndPosition if specified skipErrors", () => {
-    const legs = [
-      {
-        StartPosition: positions[0],
-      },
-      {
-        StartPosition: positions[1],
-        EndPosition: positions[2],
-      },
-      {
-        StartPosition: positions[2],
-        EndPosition: positions[3],
-      },
-      {
-        StartPosition: positions[3],
-        EndPosition: positions[4],
-      },
-    ] as Leg[];
-    const lineString = convertLegsToLineString(legs, true);
-    expectLineString(lineString).toHavePositions(positions.slice(1));
-  });
-  it("should skip following legs with undefined EndPosition if specified skipErrors", () => {
-    const legs = [
-      {
-        StartPosition: positions[0],
-        EndPosition: positions[1],
-      },
-      {
-        StartPosition: positions[1],
-        EndPosition: positions[2],
-      },
-      {
-        StartPosition: positions[2],
-      },
-      {
-        EndPosition: positions[4],
-      },
-    ] as Leg[];
-    const lineString = convertLegsToLineString(legs, true);
-    expectLineString(lineString).toHavePositions([positions[0], positions[1], positions[2], positions[4]]);
+    expect(() => convertLegsToLineString(legs)).toThrow(
+      "leg.Geometry is undefined, please make sure CalculateRoute is called with IncludeLegGeometry: true",
+    );
   });
 });
--- src/geojson/line-string-converter.ts
+++ src/geojson/line-string-converter.ts
@@ -8,49 +8,26 @@ import { Leg } from "@aws-sdk/client-location";
  * Convert an array of Amazon Location Legs to a GeoJSON LineString. It will assume the legs are connected and
  * get StartPosition and EndPosition from the first leg then connect only the EndPosition of the following legs.
  *
+ * Note: <b>IncludeLegGeometry</b> should be set to true when calling CalculateRoute or Geometry will not be present in the legs responded.
+ *
  * @param legs an array of Legs returned by AmazonLocation, such as CalculateRouteResponse.Legs
- * @param skipErrors skip a Leg if it is missing required position. An Error will be thrown by default
  * @returns A GeoJSON LineString representing the legs connected together.
  *
  * @example
  * const response = await locationClient.send(command); // command is a CalculateRouteCommand
  * const route = convertLegsToLineString(response?.Legs); // returns a LineString representing the calculated route.
  */
-export function convertLegsToLineString(legs?: Leg[], skipErrors?: boolean): LineString | undefined {
+export function convertLegsToLineString(legs?: Leg[]): LineString | undefined {
   if (legs) {
-    const throwOrSkip = (message: string, positions: Position[]): Position[] => {
-      if (skipErrors) {
-        return positions;
-      } else {
-        throw new Error(message);
-      }
-    };
-
-    const coordinates = legs.reduce((positions: Position[], leg, index): Position[] => {
+    const coordinates = legs.reduce((positions: Position[], leg): Position[] => {
       const legGeometry = leg?.Geometry?.LineString;
-      if (positions.length == 0) {
-        if (legGeometry) {
-          // Use leg geometry if possible
-          return [...legGeometry];
-        } else if (leg.StartPosition && leg.EndPosition) {
-          // Fallback to a line between start position and end position
-          return [leg.StartPosition, leg.EndPosition];
-        } else {
-          // No valid information in this leg
-          return throwOrSkip(`Leg ${index} is missing StartPosition or EndPosition.`, positions);
-        }
-      } else {
-        if (legGeometry) {
-          // Use leg geometry if possible
-          return [...positions, ...legGeometry.splice(1)];
-        } else if (leg.EndPosition) {
-          // Fallback to append the end position of the leg
-          return [...positions, leg.EndPosition];
-        } else {
-          // No valid information in this leg
-          return throwOrSkip(`Leg ${index} is missing EndPosition.`, positions);
-        }
+      if (!legGeometry) {
+        throw new Error(
+          "leg.Geometry is undefined, please make sure CalculateRoute is called with IncludeLegGeometry: true",
+        );
       }
+
+      return [...positions, ...legGeometry.splice(positions.length == 0 ? 0 : 1)];
     }, [] as Position[]);
 
     return {
