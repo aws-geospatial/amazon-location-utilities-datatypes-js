301d3ffca54395d92477db82ed6ff9cc27c09d3e
Raymond Lee <imraylee@amazon.com>
2023-06-22 16:32:38 -0700

    Add attribution document containing dependencies

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -5,6 +5,11 @@ import resolve from "@rollup/plugin-node-resolve";
 import babel from "@rollup/plugin-babel";
 
 const extensions = [".js", ".ts"];
+const banner = `
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+// Third part license at https://github.com/aws-geospatial/amazon-location-utilities-datatypes-js/blob/main/LICENSE-THIRD-PARTY.txt
+`;
 
 export default {
   input: "./src/index.ts",
@@ -21,15 +26,18 @@ export default {
     {
       file: "dist/js/index.js",
       format: "cjs",
+      banner,
     },
     {
       file: "dist/es/index.js",
       format: "esm",
+      banner,
     },
     {
       file: "dist/index.js",
       format: "iife",
       name: "amazonLocationDataConverter",
+      banner,
     },
   ],
 };
